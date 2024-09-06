accea4006a11ca445f21a67dd1fe355be4b70e63
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-09-06 18:03:53 +0900

    Create fakenitro.js

---
+++ modules/slashCommands/fakenitro.js
@@ -0,0 +1,64 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const cooldown = require('../events/cooldown');
+const handleSlashCommandError = require('../error/slashCommandError');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('fakenitro')
+        .setDescription('フェイクのNitroリンクの生成')
+        .addStringOption(option =>
+            option.setName('type')
+                .setDescription('リンクの種類を選択')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Nitroギフト形式', value: 'nitro' },
+                    { name: 'プロモNitro形式', value: 'promo' }))
+        .addIntegerOption(option =>
+            option.setName('count')
+                .setDescription('生成する数')
+                .setRequired(true)),
+
+    async execute(interaction) {
+        const commandName = this.data.name;
+        const isCooldown = cooldown(commandName, interaction);
+        if (isCooldown) return;
+
+        try {
+            await interaction.deferReply();
+            const quantity = interaction.options.getInteger('count');
+            if (quantity > 10) {
+                return interaction.editReply('生成する数は10以下にしてください');
+            }
+
+            const type = interaction.options.getString('type');
+            const nitroLinks = generateNitroLinks(quantity, type);
+
+            const embed = new EmbedBuilder()
+                .setColor('#f47fff')
+                .setTimestamp()
+                .setFooter({ text: 'Emubot | fake nitro', iconURL: interaction.client.user.displayAvatarURL() })
+                .setDescription(`**Fake ${type === 'nitro' ? 'Nitro Gift' : 'Promo Nitro'} Links** <a:nitronitronitro:1240815301801545789>\n${type === 'nitro' ? 'Nitroギフトリンク' : 'プロモNitroリンク'}\n${nitroLinks.join('\n')}`);
+
+            await interaction.editReply({ embeds: [embed] });
+        } catch (error) {
+        handleSlashCommandError(interaction.client, interaction, error);
+        }
+    },
+};
+
+function generateNitroLinks(quantity, type) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const nitroLinks = [];
+
+    for (let j = 0; j < quantity; j++) {
+        let code = Array.from({ length: type === 'nitro' ? 16 : 24 }, () => characters.charAt(Math.floor(Math.random() * characters.length))).join('');
+
+        if (type === 'promo') {
+            code = code.match(/.{1,4}/g).join('-');
+        }
+
+        const baseUrl = type === 'nitro' ? 'https://discord.gift/' : 'https://discord.com/billing/promotions/';
+        nitroLinks.push(`${baseUrl}${code}`);
+    }
+    return nitroLinks;
+}
