9a3a60e3b087e7d301db9bb7afe0e875f3a466c2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-09-06 15:54:36 +0900

    Create bancount.js

---
+++ modules/slashCommands/bancount.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
+const cooldown = require('../events/cooldown');
+const handleSlashCommandError = require('../error/slashCommandError');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bancount')
+        .setDescription('BANされているユーザーのカウント'),
+
+    async execute(interaction) {
+        try {
+            const commandName = this.data.name;
+            const isCooldown = cooldown(commandName, interaction);
+            if (isCooldown) return;
+            
+　　　　　　await interaction.deferReply();
+
+            if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+                return interaction.editReply({ content: 'botにBAN権限がないため、実行できません', ephemeral: true });
+            }
+
+            const bannedUsers = await interaction.guild.bans.fetch();
+            const bannedUsersCount = bannedUsers.size;
+
+            const embed = new EmbedBuilder()
+                .setColor('#f8b4cb')
+                .setTitle('Banned Users')
+                .setTimestamp()
+                .setFooter({ text: 'Emubot | bancount', iconURL: interaction.client.user.displayAvatarURL() })
+                .setDescription(`このサーバーのBANユーザー数: ${bannedUsersCount}`);
+
+            await interaction.editReply({ embeds: [embed] });
+        } catch (error) {
+            handleSlashCommandError(interaction.client, interaction, error);
+        }
+    },
+};
