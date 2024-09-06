125645a9e2fa8d0575177c3c9faee41f927b70a2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-09-06 16:55:42 +0900

    Update interactionError.js

--- modules/error/interactionError.js
+++ modules/error/interactionError.js
@@ -6,22 +6,20 @@ module.exports = (client, interaction, error) => {
 
     const errorEmbed = new EmbedBuilder()
         .setTitle(`Error: ${error.name}`) 
-        .setColor('#FF0000')
-        .setDescription('An error occurred during interaction processing.')
+        .setColor('Red')
+        .setDescription('interaction処理中にエラーが発生姉妹sた')
         .addFields(
             { name: 'Error', value: `\`\`\`${error.message}\`\`\`` },
             { name: 'Interaction Type', value: `${interaction.type}`, inline: true },
-            { name: 'Interaction User', value: `${interaction.user.tag}`, inline: true },
+            { name: 'server', value: message.guild.name, inline: true },
             { name: 'Time', value: new Date().toLocaleString(), inline: true }
         )
-        .setFooter({ text: `Occurred in ${interaction.guild.name}` })
-        .setTimestamp()
-        .setFooter({ text: `Emubot | ${interaction.commandName}`, iconURL: client.user.displayAvatarURL() });
-
+        .setFooter({ text: `Emubot |　${interaction.commandName}`, iconURL: client.user.displayAvatarURL() });
+    
     const errorChannel = client.channels.cache.get(channelId);
     if (errorChannel) {
         errorChannel.send({ embeds: [errorEmbed] });
     } else {
-        console.error('Error channel not found.');
+        console.error('エラーログチャンネルが見つかりません');
     }
 };
