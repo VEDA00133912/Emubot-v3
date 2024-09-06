eee5368d78de8a0fdd475d7a318d9e13255fa250
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-09-06 21:49:05 +0900

    Update slashCommandError.js

--- modules/error/slashCommandError.js
+++ modules/error/slashCommandError.js
@@ -4,9 +4,9 @@ const config = require('../../data/config.json');
 module.exports = async function handleSlashCommandError(client, interaction, error) {
     try {
         if (interaction.deferred || interaction.replied) {
-            await interaction.followUp({ content: 'コマンドの実行中にエラーが発生しました。', ephemeral: true });
+            await interaction.followUp({ content: `\`/${interaction.commandName}\`の実行中にエラーが発生しました。`, ephemeral: true });
         } else {
-            await interaction.reply({ content: 'コマンドの実行中にエラーが発生しました。', ephemeral: true });
+            await interaction.reply({ content: `\`/${interaction.commandName}\`の実行中にエラーが発生しました。`, ephemeral: true });
         }
     } catch (followUpError) {
         console.error('followupメッセージの送信に失敗しました:', followUpError);
