640ba7ecd689f2759146db88eae405753bfb97ff
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-09-06 16:59:27 +0900

    Update textCommandError.js

--- modules/error/textCommandError.js
+++ modules/error/textCommandError.js
@@ -5,6 +5,7 @@ const config = require('../../data/config.json');
 module.exports = async function handleTextCommandError(client, message, error, fileName) {
     try {
         await message.channel.send('エラーが発生しました。');
+
         const FileName = path.basename(fileName);
 
         const errorEmbed = new EmbedBuilder()
@@ -14,7 +15,7 @@ module.exports = async function handleTextCommandError(client, message, error, f
             .addFields(
                 { name: 'Error', value: `\`\`\`${error.message}\`\`\`` },
                 { name: 'command', value: FileName, inline: true },
-                { name: 'user', value: message.author.tag, inline: true },
+                { name: 'server', value: message.guild.name, inline: true },
                 { name: 'channel', value: message.channel.name, inline: true }
             )
             .setTimestamp()
@@ -29,7 +30,7 @@ module.exports = async function handleTextCommandError(client, message, error, f
             console.error('エラーログチャンネルが見つかりません。');
         }
 
-        console.error(`[${message.content}] テキストコマンド実行中にエラーが発生しました:`, error);
+        console.error(`[${FileName} テキストコマンド実行中にエラーが発生しました:`, error);
     } catch (followUpError) {
         console.error('エラーハンドリングの実行中にエラーが発生しました:', followUpError);
     }
