df4ff9462bae3c3d51c7841e76a21e3f1baee926
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-09-06 15:51:14 +0900

    Create interaction.js

---
+++ modules/events/interaction.js
@@ -0,0 +1,17 @@
+const slashCommandError = require('../error/slashCommandError');
+
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+        if (interaction.isCommand()) {
+            try {
+                const command = client.commands.get(interaction.commandName);
+                if (!command) return;
+                await command.execute(interaction, client);
+            } catch (error) {
+                console.error('Error handling slash command interaction:', error);
+                slashCommandError(client, interaction, error);
+            }
+        }
+    },
+};
